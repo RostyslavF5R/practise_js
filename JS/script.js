0ae19c273024cd31573fd499fdb33ec050549671
Rostyslav Fostyk <49285544+RostyslavF5R@users.noreply.github.com>
2021-03-03 12:43:38 +0200

    practise 4

--- JS/script.js
+++ JS/script.js
@@ -1,66 +1,73 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Кілько кін видів?", "25");
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Кілько кін видів?", "25");
-    }
-}
-
-// start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     generes: [],
-    privat: {}
+    privat: true,
+    start: function () {
+        personalMovieDB.count = +prompt("Кілько кін видів?", "25");
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Кілько кін видів?", "25");
+        }
+        // this.count = numberOfFilms;
+        // return this.count;
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const film = prompt("Якийсь фільм шариши", ""),
+                rating = prompt("Оціни", "");
+
+            if (film === null || rating === null || film === '' || rating === '' || film.length > 50) {
+                i--;
+            } else {
+                personalMovieDB.movies[film] = rating;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            console.log("Мало кін бачив");
+        } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+            console.log("Рядовий глядач");
+        } else {
+            console.log("Якийсь трохи кіноман");
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i < 2; i++) {
+            // const answer = prompt(`Ваш улюлблений жанр ${i}`);
+            // //personalMovieDB.generes[i - 1] = prompt(`Ваш улюлблений жанр ${i}`);
+            // if (answer === null || answer === '' || !isNaN(answer)) {
+            //     i--;
+            // } else {
+            //     personalMovieDB.generes[i - 1] = answer;
+            // }
+            const answer = prompt("Ваші улюлблені жанри через кому").toLowerCase();
+            if (answer === null || answer === '' || !isNaN(answer)) {
+                i--;
+            } else {
+                personalMovieDB.generes = answer.split(', ').sort();
+            }
+
+        }
+        personalMovieDB.generes.forEach(function (item, index) {
+            console.log(`Ваш улюблений жанр ${index + 1} - це ${item}`);
+        });
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+        console.log(personalMovieDB.privat);
+    }
+
 };
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const film = prompt("Якийсь фільм шариши", ""),
-            rating = prompt("Оціни", "");
-
-        if (film === null || rating === null || film === '' || rating === '' || film.length > 50) {
-            i--;
-        } else {
-            personalMovieDB.movies[film] = rating;
-        }
-    }
-}
-
-// rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Мало кін бачив");
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-        console.log("Рядовий глядач");
-    } else {
-        console.log("Якийсь трохи кіноман");
-    }
-}
-
-// detectPersonalLevel();
-
-function showMyDB() {
-    if (personalMovieDB.privat) console.log(personalMovieDB);
-}
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        const question = prompt(`Ваш улюлблений жанр ${i}`);
-        //personalMovieDB.generes[i - 1] = prompt(`Ваш улюлблений жанр ${i}`);
-        if (question === null || question === '' || !isNaN(question)) {
-            i--;
-        } else {
-            personalMovieDB.generes[i - 1] = question;
-        }
-    }
-}
-
-writeYourGenres();
-showMyDB();
